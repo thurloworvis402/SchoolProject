6ea136198646b2ac9d541a83165824647ef00a48
thurloworvis402 <thurloworvis402@gmail.com>
2025-03-14 14:29:23 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 5;
+    if (x > 10) {
+        cout << "x is greater than 10" << endl;
+    } else {
+        cout << "x is less than or equal to 10" << endl;
+    }
+    return 0;
+}
