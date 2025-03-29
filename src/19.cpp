945564e4a0e6a05cb89ba4ba138a36dc5138c9bc
thurloworvis402 <thurloworvis402@gmail.com>
2025-03-29 06:11:27 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    float result = (float)num1 / num2;
+    cout << "Result: " << result << endl;
+    return 0;
+}
