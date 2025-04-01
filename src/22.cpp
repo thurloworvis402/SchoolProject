83e9cb04c632aa815503d6094b45647c80e7fddd
thurloworvis402 <thurloworvis402@gmail.com>
2025-04-01 19:07:55 +0000

    Add 22.cpp

---
+++ src/22.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    if (age >= 18) {
+        cout << "You are old enough to vote." << endl;
+    } else {
+        cout << "Sorry, you are not old enough to vote." << endl;
+    }
+    return 0;
+}
