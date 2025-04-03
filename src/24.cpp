c1741b0fc8ae001b6b5065e14b511fa2ff0b2d4f
thurloworvis402 <thurloworvis402@gmail.com>
2025-04-03 13:30:26 +0000

    Add 24.cpp

---
+++ src/24.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    
+    if (num1 > num2) {
+        cout << "num1 is greater than num2." << endl;
+    } else {
+        cout << "num1 is not greater than num2." << endl;
+    }
+    
+    return 0;
+}
