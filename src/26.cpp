4f1fc5001e07e43b4687576089541a676dd757c4
thurloworvis402 <thurloworvis402@gmail.com>
2025-04-05 12:28:58 +0000

    Add 26.cpp

---
+++ src/26.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 10;
+    double pi = 3.14159265358979323846;
+    cout << "The value of i is: " << i << endl;
+    cout << "The value of pi is: " << pi << endl;
+
+    return 0;
+}
