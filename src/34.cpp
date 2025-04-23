38f3a840deb850a7921e688aaf9cc31f8d1143b7
thurloworvis402 <thurloworvis402@gmail.com>
2025-04-23 10:20:55 +0000

    Add 34.cpp

---
+++ src/34.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 10) {
+        cout << "The value of x is greater than 10." << endl;
+    } else {
+        cout << "The value of x is not greater than 10." << endl;
+    }
+    return 0;
+}
